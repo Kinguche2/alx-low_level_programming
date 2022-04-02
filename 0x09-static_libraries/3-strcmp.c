47e95d9b88ede0c4886261e760478865336b698c
Kinguche2 <99538237+Kinguche2@users.noreply.github.com>
2022-04-02 05:52:42 +0100

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,17 +1,24 @@
 #include "main.h"
-
 /**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
-void _puts(char *str)
-{
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
+ *_strcmp - compares two strings
+ *@s1: A pointer to a character that will be changed
+ *@s2: A pointer to a character that will also be changed/modified/updated
+ *Return: dest
+ */
 
-	_putchar('\n');
+int _strcmp(char *s1, char *s2)
+
+{
+
+char *str_one = s1;
+char *str_two = s2;
+
+
+while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+{
+str_one++;
+str_two++;
+}
+
+return (*str_one - *str_two);
 }
