56a365c269832a7e41a219432c91c0a70e3bf7ef
Kinguche2 <99538237+Kinguche2@users.noreply.github.com>
2022-04-04 23:17:49 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
@@ -11,4 +11,4 @@ char *argstostr(int ac, char **av);
 int _strlen(char *c);
 char **strtow(char *str);
 
-#endif /*HOLBERTON_H*/
+#endif /*MAIN_H*/
