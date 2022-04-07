c9af0df350f48bab601d4b5651dfcebf0571581b
Kinguche2 <99538237+Kinguche2@users.noreply.github.com>
2022-04-07 14:19:45 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,46 +1,50 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * string_nconcat - concat two strings to n bytes
- * @s1: first string
- * @s2: second string
- * @n: number of bytes to concat of string 2
- * Return: pointer to concatenated string or NULL if it fails
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
  */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr;
-	unsigned int i, len1, len2;
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
 
-	if (!s1)
-		s1 = "";
-	if (!s2)
-		s2 = "";
+	if (new_size == old_size)
+		return (ptr);
 
-	for (len1 = 0; *(s1 + len1); len1++)
-		;
-	for (len2 = 0; *(s2 + len2); len2++)
-		;
-
-	if (len2 < n)
-		n = len2;
-
-	ptr = malloc((len1 + n + 1) * sizeof(char));
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
 	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
 		return (NULL);
 
-	for (i = 0; i < (len1 + n); i++)
-	{
-		if (i < len1)
-			ptr[i] = s1[i];
-		else
-			ptr[i] = s2[i - len1];
-	}
-	ptr[i] = '\0';
+	old_ptr = ptr;
 
-	return (ptr);
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
 }
