bf3d823882289fd8de0c61fb70856900af7eb6c9
Kinguche2 <99538237+Kinguche2@users.noreply.github.com>
2022-04-07 23:05:34 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,13 +8,14 @@
  * @b: unsigned int type
  * Return: return pointer
  */
-
 void *malloc_checked(unsigned int b)
 {
 	int *ptr;
 
 	ptr = malloc(b);
 	if (ptr == NULL)
+	{
 		exit(98);
+	}
 	return (ptr);
 }
