400ffc018b86188149214e4ec4d0cb7dc548fd3e
Uche Kingsley Ogbonna <99538237+Kinguche2@users.noreply.github.com>
2022-06-13 23:45:57 +0100

    Update calc.h

--- 0x18-dynamic_libraries/test/calc.h
+++ 0x18-dynamic_libraries/test/calc.h
@@ -2,10 +2,9 @@
 #define CALC_H
 
 
-void add(int a, int b);
-void sub(int a, int b);
-void mul(int a, int b);
-void div(int a, int b);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
 
 #endif
-
